d2a000c0817da4a67267c131026d6e10e9ba780e
Hridoy Ahmed <hridoyahamad42@gmail.com>
2025-12-05 12:03:32 +0600

    updated

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,13 @@
 import express from "express";
 import "dotenv/config";
+import http from "http";
 import cors from "cors";
 import ConnectDatabase from "../database/connection.js";
 import projectRoute from "../routes/project.routes.js";
 import contactRoute from "../routes/contact.routes.js";
 
 const app = express();
+const port = process.env.PORT || 4000;
 ConnectDatabase();
 
 app.use(express.json());
@@ -18,5 +20,8 @@ app.get("/", (req, res) => {
 
 app.use("/projects", projectRoute);
 app.use("/contact", contactRoute);
+
+const server = http.createServer(app);
+server.listen(port, () => console.log(`server is running on port ${port}`));
 
 export default app;
--- backend/src/index.js
+++
@@ -1,7 +0,0 @@
-"use client";
-import http from "http";
-import app from "./app.js";
-const port = process.env.PORT || 4000;
-
-const server = http.createServer(app);
-server.listen(port, () => console.log(`server is running on port ${port}`));
