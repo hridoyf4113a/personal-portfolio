83985a08d7b72d86ac4371e094feddf12bae7294
Hridoy Ahmed <hridoyahamad42@gmail.com>
2025-12-05 11:57:09 +0600

    updated

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,7 @@
-'use client'
+"use client";
 import http from "http";
 import app from "./app.js";
-const port = process.env.PORT || 4001;
+const port = process.env.PORT || 4000;
 
 const server = http.createServer(app);
 server.listen(port, () => console.log(`server is running on port ${port}`));
